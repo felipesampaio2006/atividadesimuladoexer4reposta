2e7566c7ad02adc9e2c40be791dc56429a0b02b5
felipe Sampaio <samlipe2006@gmail.com>
2021-06-28 16:11:45 -0300

    atividade do simulado feito dia 28/06/2021

---
+++ src/atividadesimuladoexer4reposta/AtividadeSimuladoEXER4reposta.java
@@ -0,0 +1,22 @@
+package atividadesimuladoexer4reposta;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Felipe
+ */
+public class AtividadeSimuladoEXER4reposta {
+    public static void main(String[] args) {
+        int cont;
+        Scanner ler = new Scanner(System.in);
+        cont = 100;
+        while(cont<=0){
+            if(cont == 2) {
+                System.out.println(cont);
+            }
+        }
+        cont++;
+    }
+    
+}
